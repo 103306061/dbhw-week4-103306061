70578d847e71614d1d81ba1d9e3d19a93981cc3d
103306061 <103306061@nccu.edu.tw>
2016-04-04 21:54:54 +0800

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a3e2b221-75e0-45c1-8f97-75acbd43d613";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=8f6fcb24-290b-461d-9d34-72ed1b3f51f0";
 function getData(cb) {
   $.ajax({
     url : API_PATH,
